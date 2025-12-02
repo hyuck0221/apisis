d5a51d66a6e0a178aed37a042779e8f1ffe5d904
심혁 <shimhyuck@simhyeog-ui-MacBookPro.local>
2025-12-02 16:59:32 +0900

    Timezone 설정

--- src/main/kotlin/com/hshim/apisis/ApisisApplication.kt
+++ src/main/kotlin/com/hshim/apisis/ApisisApplication.kt
@@ -2,10 +2,12 @@ package com.hshim.apisis
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.TimeZone
 
 @SpringBootApplication
 class ApisisApplication
 
 fun main(args: Array<String>) {
+    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
     runApplication<ApisisApplication>(*args)
 }
