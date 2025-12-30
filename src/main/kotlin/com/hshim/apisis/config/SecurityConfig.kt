014a82204054655707bff364ec3e342b472f8700
심혁 <shimhyuck@simhyeog-ui-MacBookPro.local>
2025-12-30 16:24:50 +0900

    import 추가

--- src/main/kotlin/com/hshim/apisis/config/SecurityConfig.kt
+++ src/main/kotlin/com/hshim/apisis/config/SecurityConfig.kt
@@ -5,11 +5,12 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
-
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
